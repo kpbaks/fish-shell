999e241bfaebd169aeffc1f3ee3907ea0c7bb6bf
Kristoffer Plagborg Bak Sørensen <57013304+kpbaks@users.noreply.github.com>
2024-08-29 22:35:35 +0200

    fix(completions): `fish_opt` do not suggest non existing `--longonly` long option

--- share/completions/fish_opt.fish
+++ share/completions/fish_opt.fish
@@ -1,4 +1,4 @@
-set --local CONDITION '! __fish_seen_argument --short r --long required-val --short o --long optional-val'
+set --local CONDITION 'not __fish_seen_argument --short r --long required-val --short o --long optional-val'
 
 complete --command fish_opt --no-files
 
@@ -6,7 +6,7 @@ complete --command fish_opt --short-option h --long-option help --description 'S
 
 complete --command fish_opt --short-option s --long-option short --no-files --require-parameter --description 'Specify short option'
 complete --command fish_opt --short-option l --long-option long --no-files --require-parameter --description 'Specify long option'
-complete --command fish_opt --long-option longonly --description 'Use only long option'
+complete --command fish_opt --short-option L --long-option long-only --description 'Use only long option'
 complete --command fish_opt --short-option o --long-option optional-val -n $CONDITION --description 'Don\'t require value'
 complete --command fish_opt --short-option r --long-option required-val -n $CONDITION --description 'Require value'
 complete --command fish_opt --long-option multiple-vals --description 'Store all values'
